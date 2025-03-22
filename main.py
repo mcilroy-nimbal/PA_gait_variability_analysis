a1ff84e835618c06b01649e0710dd8b200b595bb
wmcilroy <wmcilroy@uwaterloo.ca>
2025-03-21 22:35:08 -0400

    1st commit

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ subj_sum_sub = subj_sum.loc[subj_sum['sa_class'].isin(['Control', 'SuperAger'])]
 
 #Select only mean_days from 'subj_stat' colummn
 subj_sum_sub1 = subj_sum_sub[subj_sum_sub['subject_stat'] == 'mean_days']
-
+'''
 #All subjects
 wear_table = subj_sum_sub1[['ankle_wear_duration', 'wrist_wear_duration', 'chest_wear_duration']].agg(['count','mean', 'std']).T
 activity_table = subj_sum_sub1[['sedentary', 'light', 'mvpa']].agg(['count','mean', 'std']).T
@@ -32,13 +32,14 @@ sleep_table = subj_sum_sub1[['se_total','sptw_duration_total', 'sleep_duration_t
 final__by_group_table = pd.concat([wear_table, activity_table, gait_table, sleep_table])
 # Write to CSV file
 final__by_group_table.to_csv(drive + path + "AIC2025_data_ALL_results.csv")
+'''
 
 #Controls subjects
 controls = subj_sum_sub1.loc[subj_sum_sub1['sa_class'] == 'Control']
 wear_table = controls[['ankle_wear_duration', 'wrist_wear_duration', 'chest_wear_duration']].agg(['count','mean', 'std']).T
 activity_table = controls[['sedentary', 'light', 'mvpa']].agg(['count','mean', 'std']).T
 gait_table = controls[['total_steps', 'bouted_steps', 'unbouted_steps','total_walking_duration']].agg(['count','mean', 'std']).T
-sleep_table = controls[['se_total', 'sleep_duration_total']].agg(['count','mean', 'std']).T
+sleep_table = controls[['se_total', 'sptw_duration_total','sleep_duration_total']].agg(['count','mean', 'std']).T
 final_controls = pd.concat([wear_table, activity_table, gait_table, sleep_table])
 # Write to CSV file
 final_controls.to_csv(drive + path + "AIC2025_data_CONTROL_results.csv")
